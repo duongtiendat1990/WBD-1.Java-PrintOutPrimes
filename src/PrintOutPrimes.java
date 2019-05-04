c52f29c6c39794d50ccc1e857c3c171427ee3cf5
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-05-04 22:50:17 +0700

    Optimize Algorithm

--- src/PrintOutPrimes.java
+++ src/PrintOutPrimes.java
@@ -8,24 +8,24 @@ public class PrintOutPrimes {
     System.out.print("Number : ");
     long n = scanner.nextLong();
     int i;
-      if (n<2) System.out.println("None");
-      else if (n<4)
-        for(i=2;i<=n;i++)
-          System.out.printf("%4d\n",i);
-      else
-      {
-        System.out.println("   2");
-        System.out.println("   3");
-        for (i=5;i<=n;i+=2)
-          if ((i % 6 == 1 || i % 6 == 5) && primalityTest(i))
-            System.out.printf("%4d\n",i);
-      }
+    if (n < 2) System.out.println("None");
+    else if (n < 4)
+      for (i = 2; i <= n; i++)
+        System.out.printf("%d\n", i);
+    else {
+      System.out.println("2");
+      System.out.println("3");
+      for (i = 5; i <= n; i += 2)
+        if ((i % 6 == 1 || i % 6 == 5) && primalityTest(i))
+          System.out.printf("%d\n", i);
     }
+  }
 
-    public static boolean primalityTest(int n) {
-      int i;
-      for (i=3;i<=Math.sqrt(n);i++)
-        if (n%i==0) return false;
-      return true;
+  private static boolean primalityTest(int n) {
+    int i;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (i = 1; 6 * i + 1 <= Math.sqrt(n) || 6 * i - 1 <= Math.sqrt(n); i++)
+      if (n % (6 * i + 1) == 0 || n % (6 * i - 1) == 0) return false;
+    return true;
   }
 }
