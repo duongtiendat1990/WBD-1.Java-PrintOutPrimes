9f7d4055b73ceb8a5cf963f43ee08269a4fc15d2
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-05-04 17:46:26 +0700

    Print out primes

---
+++ src/PrintOutPrimes.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PrintOutPrimes {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Primes Printer!");
+    System.out.println("Please Enter number that max printed out prime must be smaller than");
+    System.out.print("Number : ");
+    int n = Math.abs(scanner.nextInt());
+    int i;
+      if (n<2) System.out.println("None");
+      else if (n<4)
+        for(i=2;i<=n;i++)
+          System.out.printf("%4d\n",i);
+      else
+      {
+        System.out.println("   2");
+        System.out.println("   3");
+        for (i=5;i<=n;i+=2)
+          if ((i % 6 == 1 || i % 6 == 5) && primalityTest(i))
+            System.out.printf("%4d\n",i);
+      }
+    }
+
+    public static boolean primalityTest(int n) {
+      int i;
+      for (i=3;i<=Math.sqrt(n);i++)
+        if (n%i==0) return false;
+      return true;
+  }
+}
