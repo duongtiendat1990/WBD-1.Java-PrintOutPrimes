8889a8ce8f12e52cfebff0d550ccbab3eb58688a
duongtiendat1990 <48908567+duongtiendat1990@users.noreply.github.com>
2019-05-10 21:21:17 +0700

    Optimize Algorithm

--- src/PrintOutPrimes.java
+++ src/PrintOutPrimes.java
@@ -1,13 +1,16 @@
+import java.util.Date;
 import java.util.Scanner;
 
 public class PrintOutPrimes {
   public static void main(String[] args) {
+    long startTime = new Date().getTime();
     Scanner scanner = new Scanner(System.in);
     System.out.println("Primes Printer!");
     System.out.println("Please Enter number that max printed out prime must be smaller than");
     System.out.print("Number : ");
     long n = scanner.nextLong();
     long i;
+    long count = 2;
     if (n < 2) System.out.println("None");
     else if (n < 4)
       for (i = 2; i <= n; i++)
@@ -15,17 +18,21 @@ public class PrintOutPrimes {
     else {
       System.out.println("2");
       System.out.println("3");
-      for (i = 5; i <= n; i += 2)
-        if ((i % 6 == 1 || i % 6 == 5) && primalityTest(i))
+      for (i = 5; count < n; i += 2)
+        if ((i % 6 == 1 || i % 6 == 5) && primalityTest(i)){
           System.out.printf("%d\n", i);
+          count++;
+        }
     }
+    long endTime = new  Date().getTime();
+    System.out.println(endTime-startTime);
   }
 
   private static boolean primalityTest(long n) {
     long i;
     if (n % 2 == 0 || n % 3 == 0) return false;
-    for (i = 6; i <= Math.sqrt(n); i+=6)
-      if (n % (i + 1) == 0 || n % (i - 1) == 0) return false;
+    for (i = 5; i <= Math.sqrt(n); i+=6)
+      if (n % i== 0 || n % (i+2) == 0) return false;
     return true;
   }
 }
